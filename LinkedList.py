04d006fdb70275a2345d21cc82147103230a1525
Kiran Makkapati <srijamk@gmail.com>
2018-05-24 23:21:01 -0400

    implemented linked list

---
+++ LinkedList.py
@@ -0,0 +1,53 @@
+class Node():
+    
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+        self.count = 0
+
+    def length(self):
+        return self.count
+
+    def append(self, value):
+        self.count += 1
+        if self.head == None:
+            self.head = Node(value)
+            return
+        
+        current = self.head
+        while current.next != None:
+            current = this.next
+        current.next = Node(value)
+        
+    def prepend(self, value):
+        self.count += 1
+        new_head = Node(value)
+        new_head.next = self.head
+        self.head = new_head
+
+    def deleteWithValue(self, value):
+        self.count -= 1
+        if self.head == None:
+            return
+
+        current = self.head
+        print(self.head.value)
+        while current.next != None:
+            if current.next.value == value:
+                current.next = current.next.next
+            current = current.next
+
+a = LinkedList()
+a.append(1)
+print(a.count)
+a.append(2)
+print(a.count)
+a.prepend(3)
+print(a.count)
+a.deleteWithValue(2)
+print(a.count)
