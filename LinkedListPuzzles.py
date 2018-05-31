6da8f769e40e1f0b6ad61edc7f9e9615051c84d2
Srija Makkapati <srijamk@gmail.com>
2018-05-30 23:08:31 -0400

    removed LinkedList dups without buffer

--- LinkedListPuzzles.py
+++ LinkedListPuzzles.py
@@ -1,6 +1,7 @@
 from LinkedList import LinkedList
 from LinkedList import Node
 
+
 def removeDupsWithBuffer(l):
     if l.head.next == None:
         return l
@@ -15,27 +16,28 @@ def removeDupsWithBuffer(l):
         head = head.next
     return l.linkedlist
 
+
 def removeDupsWithoutBuffer(l):
     print(len(l.linkedlist))
     first_pointer = l.head
     second_pointer = first_pointer.next
-    while first_pointer != second_pointer:
-        
+    while first_pointer != second_pointer and first_pointer.next != None:
+
         while second_pointer != None:
-           
+            print(first_pointer.value, second_pointer.value)
             if first_pointer.value == second_pointer.value:
                 print("hi")
-                l.deleteWithValue(second_pointer)
-            print(first_pointer.value, first_pointer.next.value, first_pointer.next.next.value)
+                l.deleteWithValue(second_pointer.value)
             second_pointer = second_pointer.next
-            #print(first_pointer.value, second_pointer.value)
+
         first_pointer = first_pointer.next
-        second_pointer = first_pointer
+        second_pointer = first_pointer.next
     return l.linkedlist
+
 
 l = LinkedList()
 l.append(1)
 l.append(2)
 l.append(2)
-#print(removeDupsWithBuffer(l))
+# print(removeDupsWithBuffer(l))
 print(removeDupsWithoutBuffer(l))
