8bdc28911fec8ece7c1b16e2598a42ecffac60e5
Srija <srijamk@gmail.com>
2018-06-26 19:27:23 -0400

    fixed value arrangement issue

--- ZeroOneKnapsack.py
+++ ZeroOneKnapsack.py
@@ -1,9 +1,23 @@
 class ZeroOneKnapsack:
+	
+	def arrange(self, weight_arr, value_arr):
+		new_weight_arr = []
+		new_value_arr = []
+		for i in range(len(weight_arr)):
+			new_value_arr.append(max(value_arr))
+			new_weight_arr.append(weight_arr[value_arr.index(max(value_arr))])
+			value_arr[value_arr.index(max(value_arr))] = min(value_arr) - 1
+		return (new_weight_arr, new_value_arr)
+
 	def solve(self, weight_arr, value_arr, weight_bound):
 		dp = [[0 for x in range(len(weight_arr))] for y in range(weight_bound)]
 
 		highest_value = 0
 		
+		new_arrs = self.arrange(weight_arr, value_arr)
+		weight_arr = new_arrs[0]
+		value_arr = new_arrs[1]
+
 		for row in range(len(dp)):
 			remainder = row + 1
 			for col in range(len(dp[0])):
